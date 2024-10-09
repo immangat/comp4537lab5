f3ab2a41c44126e10e955f2103194b811077f0c5
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-10-08 19:28:57 -0700

    update cors

--- backend/app.js
+++ backend/app.js
@@ -71,8 +71,8 @@ class App {
     }
 
     handleRequests(req, res) {
-        console.log(req.host.origin);
-        if (req.host.origin !== 'https://comp4537lab5frontend.vercel.app') {
+        console.log(req.headers.origin);
+        if (req.headers.origin !== 'https://comp4537lab5frontend.vercel.app') {
             // Allow CORS for all origins
             res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allow specific methods
